f495f8976987e75333afe94453671b5b40488730
esabirzhanov <esabirzhanov@yahoo.com>
2020-03-28 10:52:15 -0400

    added search with 'find' example

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,51 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 import './TodoList.css';
+import { configure } from '@testing-library/react';
 
 export default class TodoList extends Component {
+
+  
   render() {
     const {items, clearList, handleDelete, handleEdit} = this.props
+
+    const bands = [
+      { title: "Led Zeppelin", 
+        musicians: [{name: "John Paul Jones"}, {name:"Robert Plant"}, {name: "Jimmy Page"}, {name: "John Bonham"}]},
+      { title: "Pink Floyd", 
+        musicians: [{name: "Syd Barret"}, {name:"Roger Waters"}, {name: "Rick Wright"}, {name:"Nick Mason"}, {name:"David Gilmour"}]},
+      { title: "Beatles", 
+        musicians: [{name:"John Lennon"}, {name:"Paul McCatrney"}, {name:"Jeorge Harrison "}, {name:"Ringo Starr"}]},
+    ]
+
+    function search(musicianToFind) {
+
+      function searchBand(band) { 
+        return band.musicians.find(searchMusician)
+      }
+  
+      function searchMusician(musician) { 
+        return musician.name === musicianToFind;
+      }
+      
+      const band = bands.find(searchBand);
+      if (band !== undefined) {
+        const musician = band.musicians.find(searchMusician)
+        return {
+          groupTitle: band.title,
+          musicianName: musician.name
+        }
+      } else {
+        return {
+          groupTitle: "",
+          musicianName: ""
+        };
+      }
+    } 
+
+    const result = search("Rick Wright2");
+
+    
     return (
       <div className="list-container">
        
